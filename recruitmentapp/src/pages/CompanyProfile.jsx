9119dd8927f6fed48d0fe06cb35fac8c98e37c8e
thaqueubc <reya.ruet@gmail.com>
2020-04-17 16:02:11 -0700

    Company profile form completed

--- recruitmentapp/src/pages/CompanyProfile.jsx
+++ recruitmentapp/src/pages/CompanyProfile.jsx
@@ -11,8 +11,10 @@ export default class CompanyProfile extends React.Component {
      companyname: " ",
      email : "",
      phone : "",
-     city : "",
-     province : ""
+     Country: "",
+     province: "",
+     city: "",
+     Address: ""
     }
   }
 
@@ -36,9 +38,9 @@ export default class CompanyProfile extends React.Component {
                 <input
                   type="text"
                   id="companyname"                  
-                  className="form-control mb-4"
-                  placeholder="Company"
+                  className="form-control mb-4"        
                   value={this.state.companyname}
+                  placeholder="Company Name"
                   onChange={e => this.setState({ companyname: e.target.value })}
                 />
 
@@ -64,15 +66,16 @@ export default class CompanyProfile extends React.Component {
                   onChange={e => this.setState({ phone: e.target.value })}
                 />
 
-                <label htmlFor='city' className='font-weight-bold'>City </label>
+                
+                <label htmlFor='country' className='font-weight-bold'>Country </label>
 
                 <input
                   type="text"
-                  id="city"                  
+                  id="country"                  
                   className="form-control mb-4"
-                  placeholder="City"
-                  value={this.state.city}
-                  onChange={e => this.setState({ city: e.target.value })}
+                  placeholder="Country"
+                  value={this.state.country}
+                  onChange={e => this.setState({ country: e.target.value })}
                 />
 
                 <label htmlFor='province' className='font-weight-bold'>Province</label>
@@ -84,6 +87,28 @@ export default class CompanyProfile extends React.Component {
                   placeholder="province"
                   value={this.state.province}
                   onChange={e => this.setState({ province: e.target.value })}
+                />
+
+                <label htmlFor='city' className='font-weight-bold'>City </label>
+
+                <input
+                  type="text"
+                  id="city"                  
+                  className="form-control mb-4"
+                  placeholder="City"
+                  value={this.state.city}
+                  onChange={e => this.setState({ city: e.target.value })}
+                />
+
+                <label htmlFor='address' className='font-weight-bold'>Address </label>
+
+                <input
+                  type="text"
+                  id="address"                  
+                  className="form-control mb-4"
+                  placeholder="Address"
+                  value={this.state.address}
+                  onChange={e => this.setState({ address: e.target.value })}
                 />
 
                 <button
